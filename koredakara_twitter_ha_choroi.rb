ddb3ea5583a756ce72d21573c54010d8a80091a5
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2012-12-31 09:27:40 +0900

    fix user name. user name change favorited user to tweeted user.

--- koredakara_twitter_ha_choroi.rb
+++ koredakara_twitter_ha_choroi.rb
@@ -7,12 +7,16 @@ Plugin::create(:koredakara_twitter_ha_choroi) do
     if !@favorited[m[:id]]
       @favorited[m[:id]] = {user: u, count: 1, tweeted: false}
     end
+    if m.to_s =~ /\#koredakara_twitter_ha_choroi/
+      @favorited[m[:id]][:tweeted] = true
+    end
     count = @favorited[m[:id]][:count]
     tweeted = @favorited[m[:id]][:tweeted]
     @favorited[m[:id]] = {user: u, count: count+1, tweeted: tweeted}
     if count >= FAV_THRETHORLD && !tweeted
       @favorited[m[:id]][:tweeted] = true
-      url = "http://favstar.fm/users/#{u}/status/#{m[:id]}"
+      user = m.idname
+      url = "http://favstar.fm/users/#{user}/status/#{m[:id]}"
       Post.primary_service.update(:message => "#koredakara_twitter_ha_choroi #{url}")
     end
   end
